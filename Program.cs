8590e2dd3ece2fc43bc272d673c7ab6878adec97
dwilliams85 <54558172+dwilliams85@users.noreply.github.com>
2019-09-19 00:04:48 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,107 @@
-﻿using System;
+using System;
 
 namespace Assignment1_Fall19
 {
     class Program
     {
         static void Main(string[] args)
+            
+        int a = 1, b = 22;
+        printSelfDividingNumbers(a, b);
+
+
+        int n2 = 5;
+        printSeries(n2);
+
+        int n3 = 5;
+        printTriangle(n3);
+
+        int[] J = new int[] { 1, 3 };
+        int[] S = new int[] { 1, 3, 3, 2, 2, 2, 2, 2 };
+        int r4 = numJewelsInStones(J, S);
+        Console.WriteLine(r4);
+
+            int[] arr1 = new int[] { 1, 2, 5, 6, 7, 8, 9 };
+        int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
+        int[] r5 = getLargestCommonSubArray(arr1, arr2);
+        Console.WriteLine(r5);
+
+            solvePuzzle();
+    }
+
+    public static void printSelfDividingNumbers(int x, int y)
+    {
+        try
         {
-            Console.WriteLine("Hello World!");
+            // Write your code here
+        }
+        catch
+        {
+            Console.WriteLine("Exception occured while computing printSelfDividingNumbers()");
+        }
+    }
+
+    public static void printSeries(int n)
+    {
+        try
+        {
+            // Write your code here
+        }
+        catch
+        {
+            Console.WriteLine("Exception occured while computing printSeries()");
+        }
+    }
+
+    public static void printTriangle(int n)
+    {
+        try
+        {
+            // Write your code here
+        }
+        catch
+        {
+            Console.WriteLine("Exception occured while computing printTriangle()");
+        }
+    }
+
+    public static int numJewelsInStones(int[] J, int[] S)
+    {
+        try
+        {
+            // Write your code here
+        }
+        catch
+        {
+            Console.WriteLine("Exception occured while computing numJewelsInStones()");
+        }
+
+        return 0;
+    }
+
+    public static int[] getLargestCommonSubArray(int[] a, int[] b)
+    {
+        try
+        {
+            // Write your code here
+        }
+        catch
+        {
+            Console.WriteLine("Exception occured while computing getLargestCommonSubArray()");
+        }
+
+        return null; // return the actual array
+    }
+
+    public static void solvePuzzle()
+    {
+        try
+        {
+            // Write your code here
+        }
+        catch
+        {
+            Console.WriteLine("Exception occured while computing solvePuzzle()");
         }
     }
 }
